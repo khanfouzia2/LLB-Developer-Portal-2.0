683dd42d377f321d583f8a6b6743d6eade74da23
NIRMAL KUMAR <nirmal.sampathkumar@tuni.fi>
2019-02-11 09:07:27 +0200

    helloworld sample prog

---
+++ helloworld-nirmal/app-frontend/src/components/Example/Example.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Example extends Component {
+  constructor(props){
+    super(props);
+  }
+  render()
+  {
+    return (<div >
+        <h1>Hey, I'm a example component</h1>
+      </div>
+    );
+  }
+}
+
+export default Example;
