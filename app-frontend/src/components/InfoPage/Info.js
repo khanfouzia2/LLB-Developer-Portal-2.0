cdfbef3b0205d33f2b15595c736c24dcbabe6e6c
Sampo Ojala <Dado@eduroam-234-92.uta.fi>
2019-04-04 16:26:47 +0300

    Landing page v1.0

--- app-frontend/src/components/InfoPage/Info.js
+++ app-frontend/src/components/InfoPage/Info.js
@@ -1,38 +1,93 @@
 import React, {Component} from 'react';
 import { AuthConsumer } from '../../context/authContext';
 import './Info.css';
+import {Link} from 'react-router-dom';
+import { create } from 'domain';
+import { FRONT_PAGE_ICONTEXT_1 } from '../../config.js'
+
+// Components
+import IconText from './IconText.js';
+
 class Info extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   renderInfoButton = (userInfo) => {
+
+    const orStyle = {margin:'0.5em',fontSize:'0.8em'};
+
     if(userInfo.isAuth) {
        return <></>
     }
     else {
       return(
-          <>    
-                <a href="/register" className="btn btn-lg btn-dark" role="button">SIGN UP</a>
-                <br></br>
-                <p>or</p>
-                <a href="/login" className="btn btn-lg btn-dark" role="button">LOGIN</a>
-          </>
+          <div className="container">    
+              <div className ="row"> 
+                <div className="col-lg-3 col-sm-6 mx-auto border_border-success">
+                  <div className="text-center">
+                    <Link to="/register" className="btn btn-lg btn-success btn-block" role="button">SIGN UP</Link>
+                    <p className="" style={orStyle}>OR</p>
+                    <Link to="/login" className="btn btn-lg btn-primary btn-block" role="button">LOGIN</Link>
+                  </div>
+                </div>
+              </div>
+          </div>
         );
     }
   }
   
   render() {
+    
+
+
+
+
     return(
       <AuthConsumer>
       { ({userInfo}) => (
          <>
-          <div className="info-wrapper">
+              <div className="info-wrapper">
               <div className="App-custom-page-content">
                   <h1>Welcome to the LLB Developer Portal</h1>
                   <br/>
-                  <p>The goal of the Living Lab Bus (LLB) project is to enable and support faster development and evaluation of mobility services through a concrete, open test environment in a real public transport context.</p>
+                  <div className = "row">
+                    <div className= "col-xl-10 mx-auto">
+                  
+                  
+                      <p>The goal of the Living Lab Bus (LLB) project is to enable and support faster development and evaluation of 
+                        mobility services through a concrete, 
+                        open test environment in a real public transport context. 
+                        <br/><br/>
+                      
+                      Join the Developer Portal and create your own application for public transport using our guides, tools and APIs!</p>
+                  
+                    </div>
+                  
+                  </div>
+                 
                   <br/>
-                  <p margin>Join the Developer Portal and create your own application for public transport using our guides, tools and APIs!</p>
-                  <br/> 
+
                   {this.renderInfoButton(userInfo)}
-              </div>
+                  
+                  <h2>3 in 1 database – many ways to use</h2>
+                  
+                  <div className="row mt-5">
+                    <div className="col-xl-4">
+                    
+                      <IconText title="Create" content="Design and code your own application using our guides and APIs." imgSrc="img/create.png"/>
+                    </div>
+                    <div className="col-xl-4 ">
+                     <IconText title="Test" content="Make your app available for testing to find bugs and issues before it goes live." imgSrc="img/test.png"/>
+                    </div>
+                    <div className="col-xl-4 text-center">
+                      <IconText title="Deploy" content="Release your app into the world and follow its progress with our feedback system." imgSrc="img/deploy.png"/>
+                    </div>
+                  </div>
+                  
+                 
+                </div>
             </div>
          </>
       )}
